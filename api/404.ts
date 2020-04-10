b13191f14122a795c23b59c84fb8ea9f24568366
tehpsalmist <bstewardcodes@gmail.com>
2020-04-10 02:57:21 -0400

    404 response

---
+++ api/404.ts
@@ -0,0 +1,5 @@
+import { NowRequest, NowResponse } from '@now/node';
+
+export default async (req: NowRequest, res: NowResponse) => {
+  res.status(404).json({ status: 404, message: 'Not Found' })
+}
